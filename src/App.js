67401ca14bb7dcf5483dda2652b02f1f24fdcf2f
Igor Tricarico <igor_tricarico@hotmail.com>
2025-01-14 17:04:16 -0300

    Criação da navegação + seleção de modo escuro/claro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+  AppBar,
+  FormControlLabel,
+  IconButton,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: isDarkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme} defaultMode="light">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Portfolio
+            </Typography>
+            <Typography>{isDarkMode ? "Escuro" : "Claro"}</Typography>
+            <Switch onChange={() => setIsDarkMode(!isDarkMode)} />
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   );
 }
 
