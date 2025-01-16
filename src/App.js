470ea036a1201f9e0e3931d261ffd9c190a120cf
Igor Tricarico <igor_tricarico@hotmail.com>
2025-01-16 17:19:23 -0300

    Ajuste na forma como o react-router-dom era utilizado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import background from "./assets/images/background.png";
 import NavBar from "./components/NavBar/NavBar";
 import { Grid2, Link, Typography } from "@mui/material";
 import AppRoutes from "./components/Routes/Routes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(
@@ -23,69 +24,72 @@ function App() {
 
   return (
     <ThemeProvider theme={theme} defaultMode="light">
-      <Grid2
-        sx={{
-          flexGrow: 1,
-          backgroundImage: `url(${background})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-        }}
-      >
+      <BrowserRouter>
         <Grid2
-          container
           sx={{
-            width: "100vw",
-            height: "100vh",
+            flexGrow: 1,
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            minHeight: "100vh",
             display: "flex",
             flexDirection: "column",
             justifyContent: "space-between",
+            alignItems: "flex-end",
           }}
         >
-          <Grid2>
-            <NavBar isDarkMode={isDarkMode} onChangeDarkMode={changeMode} />
-          </Grid2>
-          <Grid2 size="grow" margin="3rem">
-            <Grid2
-              sx={{
-                height: "100%",
-                display: "flex",
-                justifyContent: "center",
-                backgroundColor: (theme) =>
-                  alpha(theme.palette.background.paper, 0.5),
-                borderRadius: "20px",
-              }}
-            >
-              <AppRoutes />
+          <Grid2
+            container
+            sx={{
+              width: "100vw",
+              height: "100vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <Grid2>
+              <NavBar isDarkMode={isDarkMode} onChangeDarkMode={changeMode} />
             </Grid2>
-          </Grid2>
-          <Grid2 sx={{ display: "flex", flexDirection: "row-reverse" }}>
-            <Grid2
-              sx={{
-                backgroundColor: (theme) => theme.palette.primary.contrastText,
-                borderRadius: "10px",
-                padding: "2px 5px 2px 5px",
-              }}
-            >
-              <Typography>
-                <Link
-                  href="https://www.flickr.com/photos/nasawebbtelescope/54088897300/in/album-72177720313923911"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  sx={{ color: (theme) => theme.palette.primary.main }}
-                >
-                  Imagem de JWST
-                </Link>
-              </Typography>
+            <Grid2 size="grow" margin="3rem">
+              <Grid2
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  backgroundColor: (theme) =>
+                    alpha(theme.palette.background.paper, 0.5),
+                  borderRadius: "20px",
+                }}
+              >
+                <AppRoutes />
+              </Grid2>
+            </Grid2>
+            <Grid2 sx={{ display: "flex", flexDirection: "row-reverse" }}>
+              <Grid2
+                sx={{
+                  backgroundColor: (theme) =>
+                    theme.palette.primary.contrastText,
+                  borderRadius: "10px",
+                  padding: "2px 5px 2px 5px",
+                }}
+              >
+                <Typography>
+                  <Link
+                    href="https://www.flickr.com/photos/nasawebbtelescope/54088897300/in/album-72177720313923911"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    sx={{ color: (theme) => theme.palette.primary.main }}
+                  >
+                    Imagem de JWST
+                  </Link>
+                </Typography>
+              </Grid2>
             </Grid2>
           </Grid2>
         </Grid2>
-      </Grid2>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,20 +1,15 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Typography>Página principal</Typography>} />
-        <Route path="/about" element={<Typography>Sobre</Typography>} />
-        <Route path="/contact" element={<Typography>Contato</Typography>} />
-        <Route
-          path="*"
-          element={<Typography>Rota não encontrada</Typography>}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Typography>Página principal</Typography>} />
+      <Route path="/about" element={<Typography>Sobre</Typography>} />
+      <Route path="/contact" element={<Typography>Contato</Typography>} />
+      <Route path="*" element={<Typography>Rota não encontrada</Typography>} />
+    </Routes>
   );
 };
 
